c10c40b912df7ecbb06558f895c30706a3d7a0f2
Bùi Văn Trường <truongbui151930@gmail.com>
2021-11-30 16:09:18 +0700

    add file

---
+++ src/main/java/org/o7planning/sbsecurity/entity/AppRole.java
@@ -0,0 +1,25 @@
+package org.o7planning.sbsecurity.entity;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+@Entity
+@Table(name = "App_Role")
+@Data
+public class AppRole {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "Role_Id", nullable = false)
+    private Long roleId;
+
+    @Column(name = "Role_Name", length = 30, nullable = false)
+    private String roleName;
+
+ }
